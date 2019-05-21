ce99e55013ec5e75fc995bda9c3429ab194c0ef1
jutente <juliano.tda@gmail.com>
2019-05-21 16:17:09 -0300

    tabela compras

--- database/migrations/2019_04_04_184051_create_compras_table.php
+++ database/migrations/2019_04_04_184051_create_compras_table.php
@@ -15,6 +15,33 @@ class CreateComprasTable extends Migration
     {
         Schema::create('compras', function (Blueprint $table) {
             $table->increments('id');
+
+            $table->string('pac', 15);
+            $table->string('modalidade', 30);
+
+            $table->date('cotacao')->nullable();
+            $table->date('reservaorcamentariainicio')->nullable();
+            $table->date('reservaorcamentariafim')->nullable();
+            $table->date('mapasigma')->nullable();
+            $table->date('autuacao')->nullable();
+            $table->date('ordenadorinicio')->nullable();
+            $table->date('ordenadorfim')->nullable();
+            $table->date('coafinicio')->nullable();
+            $table->date('coaffim')->nullable();
+            $table->date('emailtrcontrato')->nullable();
+            $table->date('minutainicio')->nullable();
+            $table->date('minutafim')->nullable();
+            $table->date('juridicoinicio')->nullable();
+            $table->date('juridicofim')->nullable();
+            $table->date('pgminicio')->nullable();
+            $table->date('pgmfim')->nullable();
+            $table->date('preparoratificacao')->nullable();
+            $table->date('assinaturaratificacaoinicio')->nullable();
+            $table->date('assinaturaratificacaofim')->nullable();
+            $table->date('publicacaoratificacao')->nullable();
+            $table->date('conclusao')->nullable();
+            $table->text('observacao')->nullable();
+
             $table->timestamps();
         });
     }
